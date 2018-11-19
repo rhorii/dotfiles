4cc9f26bdf598f39e860b3de46dd9d00cf7319ef
Ryota Horii <horii.ryota@sharp.co.jp>
2018-11-19 10:37:40 +0900

    Add rbenv init

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,8 @@
-# PATH
-test -d $HOME/.local/bin; and set -x PATH $HOME/.local/bin $PATH
-
 # golang
 set -x GOPATH $HOME
+
+# rbenv
+status --is-interactive; and source (rbenv init -|psub)
+
+# PATH
+test -d $HOME/.local/bin; and set -x PATH $HOME/.local/bin $PATH
