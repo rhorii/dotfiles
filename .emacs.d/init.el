ee458a6b168112afca36d311a308ff771e66d934
Ryota Horii <rhorii@gmail.com>
2019-01-30 19:42:18 +0900

    Unset current-language-environment to Japanese

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -29,7 +29,6 @@
  '(counsel-describe-variable-function (quote helpful-variable))
  '(counsel-mode t)
  '(css-indent-offset 2)
- '(current-language-environment "Japanese")
  '(global-auto-revert-mode t)
  '(global-company-mode t)
  '(global-flycheck-mode t)
