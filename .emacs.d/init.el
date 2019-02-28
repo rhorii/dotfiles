d4118c307f41c980e19b643dcd27d43acd33d63c
Ryota Horii <horii.ryota@sharp.co.jp>
2019-03-01 06:59:18 +0900

    Add "C-c l" key bind to 'org-store-link

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -22,6 +22,7 @@
 (global-set-key (kbd "C-c a")   'org-agenda)
 (global-set-key (kbd "C-c c")   'org-capture)
 (global-set-key (kbd "C-c d")   'docker)
+(global-set-key (kbd "C-c l")   'org-store-link)
 (global-set-key (kbd "C-c m")   'magit-status)
 (global-set-key (kbd "C-c p")   'projectile-command-map)
 (global-set-key (kbd "C-h f")   'helpful-callable)
