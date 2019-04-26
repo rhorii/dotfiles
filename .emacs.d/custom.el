05f8591e8427f85f7381e5e4c2d8e810914fb38a
Ryota Horii <horii.ryota@sharp.co.jp>
2019-04-26 09:11:09 +0900

    Cleanup

---
+++ .emacs.d/custom.el
@@ -0,0 +1,94 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(auto-save-default nil)
+ '(blink-cursor-mode nil)
+ '(column-number-mode t)
+ '(company-minimum-prefix-length 2)
+ '(company-quickhelp-mode t)
+ '(company-selection-wrap-around t)
+ '(company-show-numbers t)
+ '(confirm-kill-emacs (quote yes-or-no-p))
+ '(counsel-mode t)
+ '(create-lockfiles nil)
+ '(docker-tramp-use-names t)
+ '(electric-pair-mode t)
+ '(flycheck-python-flake8-executable "flake8")
+ '(flycheck-python-pycompile-executable "python3")
+ '(global-auto-revert-mode t)
+ '(global-company-mode t)
+ '(global-flycheck-mode t)
+ '(global-hl-line-mode t)
+ '(indent-tabs-mode nil)
+ '(ivy-mode t)
+ '(ivy-use-virtual-buffers t)
+ '(ivy-virtual-abbreviate (quote abbreviate))
+ '(ledger-post-amount-alignment-column 62)
+ '(ledger-reports
+   (quote
+    (("Balance Sheet" "%(binary) -f %(ledger-file) bal --explicit --pedantic --cleared ^資産 ^負債 ^資本")
+     ("Monthly Balance" "%(binary) -f %(ledger-file) reg --explicit --pedantic --cleared --monthly ^資産 ^負債 ^資本 --collapse")
+     ("Monthly Expence" "%(binary) -f %(ledger-file) reg --explicit --pedantic --cleared --monthly ^支出 --sort -amount")
+     ("Account Statement" "%(binary) -f %(ledger-file) reg --explicit --pedantic --cleared ^%(account)"))))
+ '(load-prefer-newer t)
+ '(make-backup-files nil)
+ '(minions-direct (quote (flycheck-mode)))
+ '(minions-mode t)
+ '(org-agenda-files (quote ("~/org")))
+ '(org-agenda-text-search-extra-files (quote (agenda-archives)))
+ '(org-babel-load-languages (quote ((emacs-lisp . t) (shell . t))))
+ '(org-capture-templates
+   (quote
+    (("t" "Task" entry
+      (file "~/org/inbox.org")
+      "* TODO %?
+%U")
+     ("n" "Note" entry
+      (file "~/org/inbox.org")
+      "* %?
+%U"))))
+ '(org-default-notes-file "~/org/inbox.org")
+ '(org-edit-src-content-indentation 0)
+ '(org-enforce-todo-checkbox-dependencies t)
+ '(org-enforce-todo-dependencies t)
+ '(org-log-done (quote time))
+ '(org-log-into-drawer t)
+ '(org-modules (quote (org-habit org-depend)))
+ '(org-outline-path-complete-in-steps nil)
+ '(org-refile-targets (quote ((org-agenda-files :maxlevel . 3))))
+ '(org-refile-use-outline-path (quote file))
+ '(org-replace-disputed-keys t)
+ '(org-src-window-setup (quote current-window))
+ '(org-tag-alist (quote (("@office" . 111) ("@home" . 104))))
+ '(org-todo-keywords
+   (quote
+    ((sequence "TODO(t)" "WAIT(w@/!)" "|" "DONE(d)" "CANCELED(@c/!)"))))
+ '(paradox-execute-asynchronously t)
+ '(paradox-github-token t)
+ '(prog-mode-hook (quote (flyspell-prog-mode hs-minor-mode)))
+ '(projectile-completion-system (quote ivy))
+ '(recentf-max-saved-items 1024)
+ '(recentf-mode t)
+ '(require-final-newline t)
+ '(ruby-insert-encoding-magic-comment nil)
+ '(savehist-mode t)
+ '(scroll-bar-mode nil)
+ '(scroll-conservatively 10000)
+ '(sh-basic-offset 2)
+ '(show-paren-mode t)
+ '(shr-use-fonts nil)
+ '(solarized-scale-org-headlines nil)
+ '(solarized-use-variable-pitch nil)
+ '(tab-width 2)
+ '(text-mode-hook (quote (turn-on-flyspell)))
+ '(tool-bar-mode nil)
+ '(winner-mode t)
+ '(x-underline-at-descent-line t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -2,60 +2,55 @@
 ;;; Commentary:
 ;;; Code:
 
-;; proxy settings
-(load (expand-file-name "proxy.el" user-emacs-directory) t)
+(progn ;     custom.el
+  (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+  (load custom-file t))
 
-;; `package'
-(require 'package)
-(add-to-list 'package-archives
-	           '("melpa" . "https://melpa.org/packages/") t)
-(add-to-list 'package-archives
-             '("org" . "https://orgmode.org/elpa/") t)
-(package-initialize)
-(setq package-enable-at-startup nil)
+(progn ;     local.el
+  (let ((local-file (expand-file-name "local.el" user-emacs-directory)))
+    (load local-file t)))
 
-;; `use-package'
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
-(require 'use-package)
+(progn ;     coding
+  (set-language-environment "Japanese")
+  (prefer-coding-system 'utf-8))
 
-;; packages
+(progn ;     font
+  (cond
+   ((eq window-system 'ns)
+    (set-face-attribute 'default nil :family "Ricty" :height 140))
+   ((eq window-system 'x)
+    (set-face-attribute 'default nil :family "Ricty" :height 120)))
+  (set-fontset-font t 'japanese-jisx0208 (font-spec :family "Ricty")))
+
+(progn ;    `package'
+  (package-initialize)
+  (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+  (add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t))
+
+(progn ;    `use-package'
+  (unless (package-installed-p 'use-package)
+    (package-refresh-contents)
+    (package-install 'use-package))
+  (require 'use-package))
+
 (use-package async
   :ensure t
   :defer t)
 
-(use-package autorevert
-  :custom
-  (global-auto-revert-mode t))
-
 (use-package company
-  :ensure t
-  :custom
-  (company-minimum-prefix-length 2)
-  (company-selection-wrap-around t)
-  (company-show-numbers t)
-  (global-company-mode t))
+  :ensure t)
 
 (use-package company-quickhelp
   :after company
-  :ensure t
-  :custom
-  (company-quickhelp-mode t))
+  :ensure t)
 
 (use-package counsel
   :after ivy
-  :ensure t
-  :custom
-  (counsel-mode t))
+  :ensure t)
 
 (use-package csv-mode
   :ensure t
   :defer t)
-
-(use-package cus-edit
-  :custom
-  (custom-file (expand-file-name "custom.el" user-emacs-directory)))
 
 (use-package docker
   :ensure t
@@ -68,26 +63,11 @@
 
 (use-package docker-tramp
   :ensure t
-  :defer t
-  :custom
-  (docker-tramp-use-names t))
+  :defer t)
 
 (use-package dockerfile-mode
   :ensure t
   :defer t)
-
-(use-package elec-pair
-  :custom
-  (electric-pair-mode t))
-
-(use-package emacs
-  :custom
-  (create-lockfiles nil)
-  (indent-tabs-mode nil)
-  (load-prefer-newer t)
-  (scroll-conservatively 10000)
-  (tab-width 2)
-  (tool-bar-mode nil))
 
 (use-package exec-path-from-shell
   :if (memq window-system '(mac ns x))
@@ -100,45 +80,16 @@
   :bind
   ("C-=" . er/expand-region))
 
-(use-package faces
-  :config
-  (cond
-   ((eq window-system 'ns)
-    (set-face-attribute 'default nil :family "Ricty" :height 140))
-   ((eq window-system 'x)
-    (set-face-attribute 'default nil :family "Ricty" :height 120)))
-  (set-fontset-font t 'japanese-jisx0208 (font-spec :family "Ricty")))
-
-(use-package files
-  :custom
-  (auto-save-default nil)
-  (confirm-kill-emacs 'yes-or-no-p)
-  (make-backup-files nil)
-  (require-final-newline t))
-
 (use-package fish-mode
   :ensure t
   :defer t)
 
 (use-package flycheck
-  :ensure t
-  :custom
-  (flycheck-python-flake8-executable "flake8")
-  (flycheck-python-pycompile-executable "python3")
-  (global-flycheck-mode t))
+  :ensure t)
 
 (use-package flycheck-ledger
   :after (flycheck ledger-mode)
   :ensure t)
-
-(use-package flyspell
-  :hook
-  (text-mode . flyspell-mode)
-  (prog-mode . flyspell-prog-mode))
-
-(use-package frame
-  :custom
-  (blink-cursor-mode nil))
 
 (use-package helpful
   :ensure t
@@ -146,14 +97,6 @@
   ("C-h f" . helpful-callable)
   ("C-h k" . helpful-key)
   ("C-h v" . helpful-variable))
-
-(use-package hideshow
-  :hook
-  (prog-mode . hs-minor-mode))
-
-(use-package hl-line
-  :custom
-  (global-hl-line-mode t))
 
 (use-package hydra
   :ensure t
@@ -164,11 +107,7 @@
   ("C-x C-b" . ibuffer))
 
 (use-package ivy
-  :ensure t
-  :custom
-  (ivy-use-virtual-buffers t)
-  (ivy-virtual-abbreviate 'abbreviate)
-  (ivy-mode t))
+  :ensure t)
 
 (use-package ivy-hydra
   :after (hydra ivy)
@@ -182,13 +121,6 @@
 (use-package ledger-mode
   :ensure t
   :defer t
-  :custom
-  (ledger-post-amount-alignment-column 62)
-  (ledger-reports
-   '(("Balance Sheet" "%(binary) -f %(ledger-file) bal --explicit --pedantic --cleared ^資産 ^負債 ^資本")
-     ("Monthly Balance" "%(binary) -f %(ledger-file) reg --explicit --pedantic --cleared --monthly ^資産 ^負債 ^資本 --collapse")
-     ("Monthly Expence" "%(binary) -f %(ledger-file) reg --explicit --pedantic --cleared --monthly ^支出 --sort -amount")
-     ("Account Statement" "%(binary) -f %(ledger-file) reg --explicit --pedantic --cleared ^%(account)")))
   :config
   (add-hook 'ledger-mode-hook
             (lambda() (add-hook 'before-save-hook
@@ -204,107 +136,26 @@
   :defer t)
 
 (use-package minions
-  :ensure t
-  :custom
-  (minions-direct '(flycheck-mode))
-  (minions-mode t))
-
-(use-package mule
-  :config
-  (set-language-environment "Japanese")
-  (prefer-coding-system 'utf-8))
+  :ensure t)
 
 (use-package org
   :ensure org-plus-contrib
   :bind
-  ("C-c l" . org-store-link)
-  :custom
-  (org-agenda-files '("~/org"))
-  (org-agenda-text-search-extra-files '(agenda-archives))
-  (org-babel-load-languages '((emacs-lisp . t) (shell . t)))
-  (org-default-notes-file "~/org/inbox.org")
-  (org-enforce-todo-checkbox-dependencies t)
-  (org-enforce-todo-dependencies t)
-  (org-log-done 'time)
-  (org-log-into-drawer t)
-  (org-modules '(org-info org-depend))
-  (org-outline-path-complete-in-steps nil)
-  (org-refile-targets '((org-agenda-files :maxlevel . 3)))
-  (org-refile-use-outline-path 'file)
-  (org-replace-disputed-keys t)
-  (org-tag-alist '(("@office" . ?o) ("@home" . ?h)))
-  (org-todo-keywords
-   '((sequence "TODO(t)" "WAIT(w@/!)" "|" "DONE(d)" "CANCELED(@c/!)"))))
-
-(use-package org-agenda
-  :bind
-  ("C-c a" . org-agenda))
-
-(use-package org-capture
-  :bind
+  ("C-c a" . org-agenda)
   ("C-c c" . org-capture)
-  :custom
-  (org-capture-templates
-   '(("t" "Task" entry (file "~/org/inbox.org") "* TODO %?\n%U")
-     ("n" "Note" entry (file "~/org/inbox.org") "* %?\n%U"))))
-
-(use-package org-src
-  :defer t
-  :custom
-  (org-edit-src-content-indentation 0)
-  (org-src-window-setup 'current-window))
+  ("C-c l" . org-store-link))
 
 (use-package paradox
   :ensure t
-  :defer t
-  :custom
-  (paradox-execute-asynchronously t)
-  (paradox-github-token t))
-
-(use-package paren
-  :custom
-  (show-paren-mode t))
+  :defer t)
 
 (use-package projectile
   :ensure t
   :demand t
   :bind-keymap
   ("C-c p" . projectile-command-map)
-  :custom
-  (projectile-completion-system 'ivy)
-  (projectile-mode t))
-
-(use-package recentf
-  :custom
-  (recentf-max-saved-items 512)
-  (recentf-mode t))
-
-(use-package ruby-mode
-  :defer t
-  :custom
-  (ruby-insert-encoding-magic-comment nil))
-
-(use-package savehist
-  :custom
-  (savehist-mode t))
-
-(use-package scroll-bar
-  :custom
-  (scroll-bar-mode nil))
-
-(use-package sh-script
-  :defer t
-  :custom
-  (sh-basic-offset 2))
-
-(use-package shr
-  :defer t
-  :custom
-  (shr-use-fonts nil))
-
-(use-package simple
-  :custom
-  (column-number-mode t))
+  :config
+  (projectile-mode +1))
 
 (use-package smex
   :ensure t
@@ -313,10 +164,6 @@
 (use-package solarized-theme
   :if window-system
   :ensure t
-  :custom
-  (solarized-scale-org-headlines nil)
-  (solarized-use-variable-pitch nil)
-  (x-underline-at-descent-line t)
   :config
   (load-theme 'solarized-dark t))
 
@@ -328,25 +175,21 @@
 
 (use-package undo-tree
   :ensure t
-  :custom
-  (global-undo-tree-mode t))
+  :config
+  (global-undo-tree-mode +1))
 
 (use-package which-key
   :ensure t
-  :custom
-  (which-key-mode t))
+  :config
+  (which-key-mode +1))
 
 (use-package windmove
   :config
   (windmove-default-keybindings))
 
-(use-package winner
-  :custom
-  (winner-mode t))
-
 (use-package yasnippet
   :ensure t
-  :custom
+  :config
   (yas-global-mode t))
 
 (use-package yasnippet-snippets
