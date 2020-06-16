1bc2a3bbe18ef141eb1e9103600b1578bc031a66
Ryota Horii <horii.ryota@sharp.co.jp>
2020-06-17 08:45:55 +0900

    Enable indent

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -258,6 +258,7 @@
   (org-replace-disputed-keys          . t)
   (org-reverse-note-order             . t)
   (org-src-window-setup               . 'current-window)
+  (org-startup-indented               . t)
   (org-tag-alist                      . '(("@office" . ?o)
                                           ("@home"   . ?h)))
   (org-todo-keywords                  . '((sequence "TODO(t)" "WAIT(w@/!)" "|" "DONE(d)" "CANCELED(@c/!)")))
