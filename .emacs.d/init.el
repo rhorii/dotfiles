c3b341ca4469ba4dc3ff6ff22ec789a9ec68cb1d
Ryota Horii <horii.ryota@sharp.co.jp>
2020-10-29 13:32:11 +0900

    Display magit buffer same window

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -200,7 +200,8 @@
 
 (leaf magit
   :ensure t
-  :bind (("C-x g" . magit-status)))
+  :bind (("C-x g" . magit-status))
+  :custom ((magit-display-buffer-function . 'magit-display-buffer-same-window-except-diff-v1)))
 
 (leaf markdown-mode
   :ensure t
